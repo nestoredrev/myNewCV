c95db5b677bc42d903e1a999fec3358bd46b6a77
Nestor <nestor.edrev@gmail.com>
2026-02-17 13:07:50 +0100

    Add Swiper.js for enhanced certification display; update package dependencies and improve layout in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
 import {TranslateModule} from "@ngx-translate/core";
 import {TranslateService} from "@ngx-translate/core";
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  styles: `
+    .swiper{
+      width: 100%;
+      height: 100%;
+    }
+  `
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  swiperDiv = viewChild.required<ElementRef>('swiperDiv');
+  swiper:Swiper|undefined = undefined;
+
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['es', 'en', 'bg']);
     this.translate.setDefaultLang('es');
@@ -21,4 +37,24 @@ export class AppComponent {
     const lang = target.value;
     this.translate.use(lang);
   }
+
+  ngAfterViewInit(): void {
+    this.swiperInit();
+  }
+
+  swiperInit() {
+    this.swiper = new Swiper(this.swiperDiv().nativeElement, {
+      modules: [Navigation, Pagination, Scrollbar],
+      speed: 500,
+      loop: true,
+      direction: 'horizontal',
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: { el: '.swiper-pagination', clickable: true },
+      scrollbar: { el: '.swiper-scrollbar', draggable: true },
+    });
+  }
+
 }
